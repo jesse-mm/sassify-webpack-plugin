ab9a8f2626b9af3b4bf73c58ebf642e2a7696c6e
Jesse Linthorst <jesse.linthorst@gmail.com>
2017-05-20 23:21:56 +0200

    Update SharedVariablesPlugin
    Cleaned-up implementation added async await implementation.
    Updated config according to specified interface.

--- src/SharedVariablePlugin.ts
+++ src/SharedVariablePlugin.ts
@@ -3,24 +3,26 @@ const path = require('path');
 const pify = require('pify');
 const fs = require('fs');
 const mkdirp = require('mkdirp');
-import { IConfig, IFile } from 'IConfig';
+import { IConfig, IFile } from './IConfig';
 import { Stats } from 'fs';
+import ParserFactory from './parser/ParserFactory';
+import IParser from './parser/parsers/IParser';
 
 class SharedVariablesPlugin {
-	private _debug = false;
-	private _files:Array<IFile>;
-	private _forceWrite:boolean;
+	private _config:IConfig;
 
 	constructor(config:IConfig) {
+		this._config = config;
+
 		// Pre-process incoming files
-		this._files = config.files.map(file => {
+		this._config.files.map(file => {
 			return Object.assign(file, {
 				mtime: null,
 			});
 		});
 
-		this._forceWrite = config.forceWrite;
-		this._debug = config.debug;
+		// Checks the custom parser if all valid
+		this.customParserChecks();
 	}
 
 	/**
@@ -29,152 +31,107 @@ class SharedVariablesPlugin {
 	 */
 	apply(compiler:any) {
 		// Listen to the make state of the compiler
-		compiler.plugin('make', (compilation:any, callback:()=>void) => {
-			this.getSourceFilesToProcess()
-				.then((files:Array<IFile>) => this.createOutFiles(files.filter((file:IFile) => !!file)))
-				.then(() => callback());
+		compiler.plugin('make', async (compilation:any, callback:()=>void) => {
+			const sourceFiles = await (this.getSourceFilesToProcess());
+			await this.processFiles(sourceFiles);
+			callback();
 		});
+	}
+
+	private isArray = function(a:any) {
+		return (!!a) && (a.constructor === Array);
+	};
+
+	private customParserChecks() : void {
+		// Simple helper function for checking string
+		const isString = ((fileExtension:string) => typeof fileExtension === 'string');
+
+		// Pre-process customParser
+		if (this._config.customParser) {
+			if (typeof this._config.customParser.parser !== 'function') {
+				throw new Error('[SharedVariablesPlugin] No valid customParser supply a method.');
+			}
+			if (!this._config.customParser.fileExtension) {
+				throw new Error('[SharedVariablesPlugin] customParser object lacks a fileExtension property.')
+			} else {
+				const fileExtension = this._config.customParser.fileExtension;
+
+				if (isString(<string> fileExtension)) {
+					this._config.customParser.fileExtension = [(<string> fileExtension).toUpperCase()];
+				} else if (this.isArray(fileExtension)) {
+					(<Array<string>> fileExtension).forEach((extension: string) => {
+						if (!isString(extension)) {
+							throw new Error('[SharedVariablesPlugin] No valid file extension in Array.');
+						}
+					});
+
+					this._config.customParser.fileExtension = (<Array<string>> fileExtension)
+						.map((extension: string) => extension.toUpperCase());
+				} else {
+					throw new Error(`[SharedVariablesPlugin] customParser has no valid fileExtension must be one of
+					Array or String.`);
+				}
+			}
+		}
 	}
 
 	/**
 	 * Returns files to be processed from JS to SCSS equivalent.
 	 * @returns {Promise.<Array>}
 	 */
-	private getSourceFilesToProcess() : any {
-		const promises = new Array(this._files.length);
+	private async getSourceFilesToProcess() {
+		const files:Array<IFile> = [];
 
-		for (let i = 0; i < this._files.length; i++) {
-			const file = this._files[i];
+		for (let i = 0; i < this._config.files.length; i++) {
+			const file = this._config.files[i];
+			const mtime = await this.getFileModificationTime(file);
 
-			promises[i] = this.statSourceFile(file).then((mtime:string) => {
-				if( file.mtime !== mtime) {
-					file.mtime = mtime;
-					return file;
-				}
-			});
+			if( file.mtime !== mtime) {
+				file.mtime = mtime;
+				files.push(file);
+			}
 		}
 
-		return Promise.all<IFile>(promises);
+		return files;
 	}
 
 	/**
 	 * Stats the in file
 	 * @param file
 	 */
-	statSourceFile(file:IFile) {
+	private getFileModificationTime(file:IFile) {
 		return pify(fs.stat)(path.resolve(file.source))
 			.then((statResult:Stats) => {
-				console.log(statResult);
 				return statResult.mtime.getTime();
 			}).catch((statError:Error) => {
 				throw statError;
 			});
 	}
 
-	/**
-	 * Parses and evaluates the content that was read
-	 * @param fileData
-	 * @returns {*}
-	 */
-	parseSourceFile(fileData:string) {
-		// Remove tabs, split on newlines
-		let lines = fileData.replace(/\t/, '').split(/\n/);
-		// Remove first line and last line of the file
-		lines.splice(0,1);
-		lines.splice(-1);
-		// Join remaining lines
-		let lineResult = lines.join('');
-
-		// Check if lines end with a bracket (When user saved without empty line on EOF)
-		if (lineResult[lines.length - 1] === '}') {
-			lineResult = `{${lineResult}`;
-		} else {
-			lineResult = `{${lineResult}}`;
-		}
-
-		let compiledSource = null;
-
-		try {
-			// Danger danger high voltage (Compile into object)
-			compiledSource = eval(`'use strict'; const compile = ${lineResult}; compile`);
-		}
-		catch(error) {
-			if (this._debug) {
-				console.error(error);
-			}
-		}
-
-		return compiledSource;
+	private getFileExtension(filePath:string) : string {
+		return filePath.split(/\./g).pop().toUpperCase();
 	}
 
-	/**
-	 * Generates source code for scss file based on compiledSource
-	 * @param compiledSource
-	 * @param fileInName
-	 * @param fileOutName
-	 * @returns {string}
-	 */
-	generateDestinationFile(compiledSource:{[index:string]:string}, fileInName:string, fileOutName:string) {
-		if( compiledSource === null || typeof compiledSource !== 'object') {
-			console.error(`[${fileInName}] Cannot generate outFile. Something seems wrong with the compiled source...`);
-			return;
-		}
+	private async processFiles(files:Array<IFile>) {
+		const parsers:Array<IParser> = new Array(files.length);
 
-		const mapName = fileOutName.split(/\./).shift();
-
-		const prepend = `$${mapName}: (\n`;
-		const content = Object.keys(compiledSource)
-			.map(key => `\t${key.toUpperCase()}: '${compiledSource[key]}',`)
-			.join("\n");
-		const append = '\n);';
-
-		return `${prepend}${content}${append}`;
-	}
-
-	/**
-	 * Iterates through all files and processes them to create the out files
-	 * @param files
-	 * @returns {Promise.<*>}
-	 */
-	createOutFiles(files:Array<IFile>) {
-		if( files.length === 0) {
-			return Promise.resolve();
-		}
-
-		const promises = new Array(files.length);
-		const getFileName = (fileName:string) => fileName.split(/\//).pop();
+		const customParser = this._config.customParser;
 
 		for (let i = 0; i < files.length; i++) {
 			const file = files[i];
-			promises[i] = pify(fs.readFile)(path.resolve(file.source), 'utf-8')
-				.then((fileData:string) => this.parseSourceFile(fileData))
-				.then((compiledJs:{[index:string]:string}) => this.generateDestinationFile(
-					compiledJs,
-					getFileName(file.source),
-					getFileName(file.dest))
-				)
-				.then((scssSource:string) => this.writeDestinationFile(scssSource, file.dest))
-				.catch((error:Error) => console.error(error));
-		}
+			const fileExtension = this.getFileExtension(file.source);
 
-		return Promise.all(promises);
+			if (customParser && customParser.fileExtension.indexOf(fileExtension) > -1) {
+				console.log(`[Sassify] Using custom parser for ${fileExtension}`);
+				parsers[i] = this._config.customParser.parser;
+			} else {
+				parsers[i] = new ParserFactory(file).parser;
+			}
+
+			await parsers[i].run();
+		}
 	}
 
-	/**
-	 * Writes the SCSS file
-	 * @param scssSource
-	 * @param outFile
-	 */
-	writeDestinationFile(scssSource:string, outFile:string) {
-		const fileToWrite:()=>Promise<any> = () => pify(fs.writeFile)(path.resolve(outFile), scssSource, 'utf8');
-
-		if (this._forceWrite) {
-			return pify(mkdirp)(path.dirname(path.resolve(outFile)))
-				.then(<any> fileToWrite());
-		}
-
-		return fileToWrite();
-	}
 }
 
 module.exports = SharedVariablesPlugin;
