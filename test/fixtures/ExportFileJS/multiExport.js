f672e37e7ec8870d033360dd6f8e3fa539e45438
Jesse Linthorst <jesse.linthorst@gmail.com>
2017-05-27 12:58:01 +0200

    Add disabling sassify for specific lines

--- test/fixtures/ExportFileJS/multiExport.js
+++ test/fixtures/ExportFileJS/multiExport.js
@@ -4,6 +4,18 @@ export const mediaQueries = {
 	LARGE: '(min-width: 1024px)',
 };
 
+// sassify-disable
+export const dontSassifyMe = {
+	INTERNAL_USED_VAR: 0,
+};
+// sassify-enable
+
+//sassify-disable
+export const concatSingle = {
+	INTERNAL_USED_VAR: 'foo'
+};
+//sassify-enable
+
 export const colors = {
 	starlightBlue: '#08BECD',
 	lorna: '#67C5CD',
@@ -11,6 +23,26 @@ export const colors = {
 	oceanStorm: '#063E54',
 	weakest: '358391',
 };
+
+/* sassify-disable */
+export const dontSassifyMeToo = {
+	INTERNAL_USED_VAR: 'foo',
+};
+/* sassify-enable */
+
+/*sassify-disable*/
+export const testConcatComment = {
+	INTERNAL_USED_VAR: 'foo',
+};
+/*sassify-enable*/
+
+/** sassify-disable
+ *  I've disabled sassify here this because I could!
+ * */
+export const testMultiLineComment = {
+	INTERNAL_USED_VAR: 'foo',
+};
+/* sassify-enable */
 
 export const pallete = {
 	ohBabe: {
