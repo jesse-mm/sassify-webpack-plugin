fbe81804cfb598c571bc6bfe3bada76e42671e4f
Jesse Linthorst <jesse.linthorst@gmail.com>
2017-06-05 14:50:03 +0200

    Add compile-src to build script

--- bin/build.js
+++ bin/build.js
@@ -8,7 +8,7 @@ const chalk = require('chalk');
 
 	const build = {
 		clean: () => shelljs.rm('-Rf', `${rootDir}/dist/`),
-		compile: () => shelljs.exec(`${rootDir}/node_modules/.bin/tsc -p ${rootDir}`),
+		compile: () => shelljs.exec(`${rootDir}/node_modules/.bin/tsc -p ${rootDir} --outDir ${rootDir}/dist`),
 		create: () => shelljs.mkdir(`${rootDir}/dist/parser/temp`),
 		copy: () => shelljs.cp('-R', `${rootDir}/src/parser/template`, `${rootDir}/dist/parser/template`),
 		done: () => console.log('> ' + chalk.white.bgGreen.bold(`${type} executed`)),
@@ -17,6 +17,11 @@ const chalk = require('chalk');
 	switch(type[0]) {
 		case 'build': {
 			Object.keys(build).forEach(key => build[key]());
+			break;
+		}
+		case 'compile-src': {
+			shelljs.exec(`${rootDir}/node_modules/.bin/tsc -p ${rootDir} --outDir ${rootDir}src/`);
+			build.done();
 			break;
 		}
 		case 'clean': {
